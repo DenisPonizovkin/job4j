e7bfa9498fc02572473c95a64af3c99abd679f8e
Denis Ponizovkin <denis.ponizovkin@gmail.com>
2018-06-06 18:09:04 +0300

    5.1.3. Создать итератор простые числа. [#151]

---
+++ chapter_001/src/main/java/ru/job4j/prime/IteratorArrayPrimeNumbers.java
@@ -0,0 +1,74 @@
+package ru.job4j.prime;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * Iterator which returns prime number.
+ */
+public class IteratorArrayPrimeNumbers implements Iterator {
+    /**
+     * Data.
+     */
+    private int[] array;
+    /**
+     * Current index.
+     */
+    private int index = 0;
+    /**
+     * If there is no prime number in array then isPrimeNumber() method return this value.
+     */
+    private final int noPrimeNumberId = -1;
+
+    public IteratorArrayPrimeNumbers(int[] array) {
+        this.array = array;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return primeNumberId() != -1;
+    }
+
+    @Override
+    public Object next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        for (++index; index < array.length; index++) {
+            if (isPrimeNumber(array[index])) {
+                return array[index];
+            }
+        }
+        throw new NoSuchElementException();
+    }
+
+    /**
+     *
+     * @return id of first prime number after index.
+     */
+    private int primeNumberId() {
+        for (int i = index + 1; i < array.length; i++) {
+           if (isPrimeNumber(array[i])) {
+               return i;
+           }
+        }
+        return noPrimeNumberId;
+    }
+
+    /**
+     *
+     * @param n - integer number.
+     * @return - return true if the n is prime, false otherwise.
+     */
+    private boolean isPrimeNumber(int n) {
+        if (n == 2) {
+            return true;
+        }
+        for (int divider = 2; divider <= n / 2; divider++) {
+            if (n % divider == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ chapter_001/src/main/java/ru/job4j/prime/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Package for iterator which returns prime numbers.
+ *
+ * @author Denis Ponizovkin (mailto: denis.ponizovkin@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+
+package ru.job4j.calculate;
