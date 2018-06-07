9615b3539eabc7216a1d5be1c622582faf9d5578
Denis Ponizovkin <denis.ponizovkin@gmail.com>
2018-06-07 09:19:12 +0300

    5.2.1. Реализовать SimpleArray<T> [#156]

---
+++ chapter_001/src/main/java/ru/job4j/generic/array/simple/SimpleArray.java
@@ -0,0 +1,120 @@
+package ru.job4j.generic.array.simple;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * Generic wrapper for array.
+ */
+public class SimpleArray<T> implements Iterable<T> {
+
+    private Object[] data;
+    int currentPosition = 0;
+
+    /**
+     * Simple array iterator.
+     * @param <T>
+     */
+    class SimpleArrayIterator<T> implements Iterator<T> {
+
+        SimpleArray<T> array;
+        int currentPosition = 0;
+
+        SimpleArrayIterator(SimpleArray<T> array) {
+            this.array = array;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return getSize() < capacity();
+        }
+
+        @Override
+        public T next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            return array.get(currentPosition++);
+        }
+    }
+
+    /**
+     * Number of elements in array.
+     * @return - number of elements.
+     */
+    public int getSize() {
+       return currentPosition;
+    }
+
+    /**
+     * Reserved size.
+     * @return - reserved size.
+     */
+    public int capacity() {
+        return data.length;
+    }
+
+    /**
+     * Constructor.
+     * @param n - number of elements in array (data member);
+     */
+    public SimpleArray(int n) {
+        data = new Object[n];
+    }
+
+    /**
+     * Add element in array.
+     * @param e - new element.
+     */
+    public void add(T e) {
+        if (currentPosition == data.length - 1) {
+           throw new ArrayStoreException();
+        }
+        data[currentPosition++] = e;
+    }
+
+    /**
+     * Set element in [index] place.
+     * @param index - index of element ini array.
+     * @param e - element.
+     */
+    public void set(int index, T e) {
+        if (index > currentPosition) {
+            throw new IndexOutOfBoundsException();
+        }
+        data[index] = e;
+    }
+
+    /**
+     * Delete element at index place.
+     * @param index - index of element.
+     */
+    public void delete(int index) {
+        if (index > currentPosition) {
+            throw new IndexOutOfBoundsException();
+        }
+        if (index < currentPosition) {
+            for (int i = index + 1; i < currentPosition; i++) {
+                data[i - 1] = data[i];
+            }
+        }
+        currentPosition--;
+    }
+
+    /**
+     * Get element from array at index position.
+     * @param index - index of element.
+     * @return element.
+     */
+    T get(int index) {
+        if (index > currentPosition) {
+           throw new IndexOutOfBoundsException();
+        }
+        return (T) data[index];
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new SimpleArrayIterator<T>(this);
+    }
+}
