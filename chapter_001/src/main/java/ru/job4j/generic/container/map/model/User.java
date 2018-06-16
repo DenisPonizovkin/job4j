98522af3fc5bf39ff4e101a5fb5120718033fda8
Denis Ponizovkin <denis.ponizovkin@gmail.com>
2018-06-16 15:32:26 +0300

    1. Создать модель User [#999]

---
+++ chapter_001/src/main/java/ru/job4j/generic/container/map/model/User.java
@@ -0,0 +1,51 @@
+package ru.job4j.generic.container.map.model;
+
+import java.util.Calendar;
+
+public class User {
+
+    private String name;
+    int children;
+    Calendar birthday;
+
+    public User(String name, int children, Calendar birthday) {
+        this.name = name;
+        this.children = children;
+        this.birthday = birthday;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getChildren() {
+        return children;
+    }
+
+    public void setChildren(int children) {
+        this.children = children;
+    }
+
+    public Calendar getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Calendar birthday) {
+        this.birthday = birthday;
+    }
+
+    public void print() {
+        System.out.printf(this.toString());
+    }
+
+    @Override
+    public String toString() {
+        return "User{" + "name='" + name + '\''
+                + ", children=" + children
+                + ", birthday=" + birthday + '}';
+    }
+}
