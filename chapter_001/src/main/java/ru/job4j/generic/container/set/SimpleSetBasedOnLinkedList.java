a1478a694f2f0b96fe84bf267a31c64b818f94e7
Denis Ponizovkin <denis.ponizovkin@gmail.com>
2018-06-16 13:08:56 +0300

    2. Set реализованный на связном списке. [#997]

---
+++ chapter_001/src/main/java/ru/job4j/generic/container/set/SimpleSetBasedOnLinkedList.java
@@ -0,0 +1,36 @@
+package ru.job4j.generic.container.set;
+
+import ru.job4j.generic.container.list.linked.LinkedListArrayBased;
+
+import java.util.Iterator;
+
+/**
+ * Simple set based on linked list.
+ * @param <E>.
+ * @author Denis Ponizovkin.
+ */
+public class SimpleSetBasedOnLinkedList<E> implements Iterable<E> {
+
+    private LinkedListArrayBased<E> data;
+
+    /**
+     * Add an element to the set.
+     * @param e
+     */
+    void add(E e) {
+        boolean is = false;
+        for (Iterator it = data.iterator(); it.hasNext()) {
+            if (it.next().equals(e)) {
+               is = true;
+            }
+        }
+        if (!is) {
+            data.add(e);
+        }
+     }
+
+    @Override
+    public Iterator<E> iterator() {
+        return data.iterator();
+    }
+}
