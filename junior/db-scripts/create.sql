58f9d62c9fc7b975ee3958fe4367beb77c818920
Denis Ponizovkin <denis.ponizovkin@gmail.com>
2018-07-26 09:17:26 +0300

    1. UML системы [#62660]

--- junior/db-scripts/create.sql
+++ junior/db-scripts/create.sql
@@ -1,3 +1,4 @@
+-- Create scheme. 1. UML системы [#62660]
 \i create_database.sql
 \i create_tables.sql
 \i fill_db.sql
--- junior/db-scripts/create_database.sql
+++ junior/db-scripts/create_database.sql
@@ -1,3 +1,5 @@
+-- Create users/roles database. 1. UML системы [#62660]
+
 CREATE DATABASE job4j;
 CREATE USER postgres WITH password 'postgres';
 GRANT ALL privileges ON DATABASE job4j TO postgres;
