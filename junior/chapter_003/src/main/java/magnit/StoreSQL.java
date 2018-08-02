6c1d7aaf89d4363d91773301456f5966d29989b4
Denis Ponizovkin <denis.ponizovkin@gmail.com>
2018-08-02 18:19:19 +0300

    4.3. XML XSLT JDBC Оптимизация [#20459][#63001]

--- junior/chapter_003/src/main/java/magnit/StoreSQL.java
+++ junior/chapter_003/src/main/java/magnit/StoreSQL.java
@@ -6,15 +6,11 @@ import java.util.List;
 
 public class StoreSQL implements AutoCloseable {
 
-    private Connection conn = null;
     private final Config cfg;
 
     @Override
     public void close() throws Exception {
-        if (conn != null) {
-            System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-            conn.close();
-        }
+        System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
     }
 
     public StoreSQL(Config cfg) {
@@ -22,9 +18,6 @@ public class StoreSQL implements AutoCloseable {
     }
 
     public void start() throws SQLException {
-        if (conn == null) {
-            conn = createConnection();
-        }
         if (!tableExists()) {
             createaTable();
         }
@@ -42,62 +35,67 @@ public class StoreSQL implements AutoCloseable {
             clear();
         }
         String sql = "insert into entry values (?)";
-        try (PreparedStatement st = conn.prepareStatement(sql)) {
-            for (int i = 1; i <= cfg.getN(); i++) {
-                st.setInt(1, i);
-                st.executeUpdate();
+        try (Connection conn = createConnection()) {
+            try (PreparedStatement st = conn.prepareStatement(sql)) {
+                for (int i = 1; i <= cfg.getN(); i++) {
+                    st.setInt(1, i);
+                    st.executeUpdate();
+                }
             }
         }
     }
 
     private void clear() throws SQLException {
-        try (PreparedStatement st = conn.prepareStatement("delete  from " + cfg.getTable())) {
-            st.executeUpdate();
-            st.close();
+        try (Connection conn = createConnection()) {
+            try (PreparedStatement st = conn.prepareStatement("delete  from " + cfg.getTable())) {
+                st.executeUpdate();
+            }
         }
     }
 
     public void createaTable() throws SQLException {
-        conn.createStatement().execute(
-                "create table "
-                        + cfg.getTable()
-                        + "( id integer primary key );");
+        try (Connection conn = createConnection()) {
+            try (Statement st = conn.createStatement()) {
+                st.execute("create table " + cfg.getTable() + " (id integer primary key);");
+            }
+        }
     }
 
     public boolean tableExists() throws SQLException {
         boolean empty = true;
-        DatabaseMetaData dmd = conn.getMetaData();
-        try (ResultSet rs = dmd.getTables(null, null, cfg.getTable(), null)) {
-            if (rs.next()) {
-                empty = false;
+        try (Connection c = DriverManager.getConnection("jdbc:sqlite:" + cfg.getBase())) {
+            DatabaseMetaData dmd = c.getMetaData();
+            try (ResultSet rs = dmd.getTables(null, null, cfg.getTable(), null)) {
+                if (rs.next()) {
+                    empty = false;
+                }
             }
-            rs.close();
         }
         return !empty;
     }
 
     public int count() throws SQLException {
         int n = 0;
-        try (Statement st = conn.createStatement();
-             ResultSet rs = st.executeQuery("select count(*) from " + cfg.getTable())) {
-            while (rs.next()) {
-                n = rs.getInt(1);
+        try (Connection conn = createConnection()) {
+            try (Statement st = conn.createStatement();
+                 ResultSet rs = st.executeQuery("select count(*) from " + cfg.getTable())) {
+                while (rs.next()) {
+                    n = rs.getInt(1);
+                }
             }
-            rs.close();
-            st.close();
         }
         return n;
     }
 
     public List<Entry> findAll() throws SQLException {
         List<Entry> list = new ArrayList<Entry>();
-        try (Statement st = conn.createStatement();
-             ResultSet rs = st.executeQuery("select * from " + cfg.getTable())) {
-            while (rs.next()) {
-                list.add(new Entry(rs.getInt(1)));
+        try (Connection conn = createConnection()) {
+            try (Statement st = conn.createStatement();
+                 ResultSet rs = st.executeQuery("select * from " + cfg.getTable())) {
+                while (rs.next()) {
+                    list.add(new Entry(rs.getInt(1)));
+                }
             }
-            rs.close();
-            st.close();
         }
         return list;
     }
