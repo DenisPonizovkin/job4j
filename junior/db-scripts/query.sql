3b8f07d63f9d169062fbc43452132245fb4e10e9
Denis Ponizovkin <denis.ponizovkin@gmail.com>
2018-08-02 14:16:16 +0300

    Фильтры [#1732]
    Хорошо. я только хотел еще увидеть group by + having.

--- junior/db-scripts/query.sql
+++ junior/db-scripts/query.sql
@@ -6,3 +6,12 @@ select count(*) from product where type_id = 1
 select * from query.product as p inner join query.type as t on p.type_id = t.id and t.name = 'СЫР'and t.name = 'МОЛОКО';
 select distinct(id) from product p inner join (SELECT type_id, COUNT(*) FROM product GROUP BY type_id) cnt on cnt.count < 10 and p.type_id = cnt.type_id
 select *, t.id from product p inner join (select * from type) t on p.type_id = t.id
+
+
+-- 6. Написать запрос получение всех продуктов с типом "СЫР" и "МОЛОКО"
+select * from
+product as p
+inner join (select * from type) as t
+on p.type_id=t.id group by p.id,t.id,t.name
+having t.name in ('СЫР', 'МОЛОКО') order by t.id
+
