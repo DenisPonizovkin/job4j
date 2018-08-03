198d644057cc150a1e0984bd3be14fe0753c9de1
Denis Ponizovkin <denis.ponizovkin@gmail.com>
2018-08-03 15:10:53 +0300

    tmp

--- junior/chapter_003/src/test/java/magnit/StoreSQLTest.java
+++ junior/chapter_003/src/test/java/magnit/StoreSQLTest.java
@@ -3,8 +3,7 @@ package magnit;
 import org.junit.Test;
 
 import javax.xml.bind.JAXBException;
-import java.sql.Connection;
-import java.sql.DriverManager;
+import java.io.File;
 import java.sql.SQLException;
 
 import static org.hamcrest.CoreMatchers.is;
@@ -12,11 +11,11 @@ import static org.junit.Assert.assertThat;
 
 public class StoreSQLTest {
 
-    //@Test
+    @Test
     public void whenStartThenNRowsExists() throws SQLException {
         System.out.println("Test 1 start");
         final int n = 5;
-        Config cfg = new Config("magnit.db", "entry", n);
+        Config cfg = new Config("mgnt.db", "entry", n);
         StoreSQL ss = new StoreSQL(cfg);
         ss.start();
 
@@ -24,19 +23,17 @@ public class StoreSQLTest {
         assertThat(ss.count(), is(n));
     }
 
-    @Test
+    //@Test
     public void whenSaveToFileNoExceptions() throws SQLException, JAXBException {
         final int n = 15;
         Config cfg = new Config("magnit.db", "entry", n);
-        try (Connection c = DriverManager.getConnection("jdbc:sqlite:" + cfg.getBase())) {
-            StoreSQL ss = new StoreSQL(cfg);
-            ss.start();
-            StoreXML sxml = new StoreXML(new File("./magnit.xml"));
-            sxml.save(ss.findAll());
+        StoreSQL ss = new StoreSQL(cfg);
+        ss.start();
+        StoreXML sxml = new StoreXML(new File("./magnit.xml"));
+        sxml.save(ss.findAll());
 
-            System.out.println("Test 2 end");
-            assertThat(true, is(true));
-        }
-   }
+        System.out.println("Test 2 end");
+        assertThat(true, is(true));
+    }
 
 }
