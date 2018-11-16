57cdcdfdb84185d5d7ddf18cf1b712f382664841
Denis Ponizovkin <denis.poizovkin@gmail.com>
2018-11-16 14:43:53 +0300

    Ajax Jquery [#58534][#83727]
    1. private fields
    2. User если это модель используется в коллекциях
    то всегда надо определять equals hashCode
    3. remove commented code
    4. writer.append("[{'fname': 'test_fname', 'sname': 'sname_test', 'sex': 'male', 'desc': 'test'}]");
    это копипаст какой то? зачем это? удалить тоже.

--- junior/ajax_jquery/src/main/java/ru/job4j/servlets/model/User.java
+++ junior/ajax_jquery/src/main/java/ru/job4j/servlets/model/User.java
@@ -2,10 +2,10 @@ package ru.job4j.servlets.model;
 
 public class User {
 
-    String fname;
-    String sname;
-    String sex;
-    String desc;
+    private String fname;
+    private String sname;
+    private String sex;
+    private String desc;
 
     public String getFname() {
         return fname;
@@ -37,5 +37,26 @@ public class User {
 
     public void setDesc(String desc) {
         this.desc = desc;
+    }
+
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        if (!super.equals(object)) return false;
+        User user = (User) object;
+        return java.util.Objects.equals(fname, user.fname) &&
+                java.util.Objects.equals(sname, user.sname) &&
+                java.util.Objects.equals(sex, user.sex) &&
+                java.util.Objects.equals(desc, user.desc);
+    }
+
+    public int hashCode() {
+        int hash = 0;
+        hash += fname.isEmpty() ? 0 : fname.hashCode();
+        hash += sname.isEmpty() ? 0 : sname.hashCode();
+        hash += sex.isEmpty() ? 0 : sex.hashCode();
+        hash += desc.isEmpty() ? 0 : desc.hashCode();
+
+        return 31 * 17 + hash;
     }
 }
